549448456bb2e628513b96499d7900be448fd8af
Kykypy3a1 <108570735+Kykypy3a1@users.noreply.github.com>
2022-07-19 19:23:18 +0300

    Поменял пом чтобы работал утил

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -6,14 +6,17 @@ import java.sql.SQLException;
 
 public class Util {
     // реализуйте настройку соеденения с БД
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+    public static void main(String[] args) {
+
+    }
+
+    public static void getConnection() throws ClassNotFoundException, SQLException {
         String url = "jdbc:mysql://localhost:3306";
         String username = "root";
         String password = "Risetotop_13";
-        Class.forName("com.mysql.jdbc.Driver");
-        try (Connection connection = DriverManager.getConnection(url,username, password)) {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        try (Connection connection = DriverManager.getConnection(url, username, password)) {
             System.out.println("we're connected");
         }
     }
-
 }
