35abe7ff29d44241f8a6b5cbe113f11a4b3182bb
Kykypy3a1 <108570735+Kykypy3a1@users.noreply.github.com>
2022-07-19 18:17:16 +0300

    Update Util.java
    сделал Util - не запускается, посмотреть что делать

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,19 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        String url = "jdbc:mysql://localhost:3306";
+        String username = "root";
+        String password = "Risetotop_13";
+        Class.forName("com.mysql.jdbc.Driver");
+        try (Connection connection = DriverManager.getConnection(url,username, password)) {
+            System.out.println("we're connected");
+        }
+    }
+
 }
