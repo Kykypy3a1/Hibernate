1630571ea6c188841fbe9e87d79f3ee6ec7317e2
Kykypy3a1 <108570735+Kykypy3a1@users.noreply.github.com>
2022-09-05 17:34:31 +0300

    Update Main.java

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -7,15 +7,15 @@ import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        UserDao userDao1 = new UserDaoHibernateImpl();
-        userDao1.createUsersTable();
-        userDao1.saveUser("ivan","petrov", (byte) 24);
-        userDao1.saveUser("petr","ivanov", (byte) 14);
-        userDao1.saveUser("name1","petrov", (byte) 35);
-        userDao1.saveUser("ilya","sidorov", (byte) 18);
-        userDao1.removeUserById(7);
-        userDao1.getAllUsers();
-        userDao1.cleanUsersTable();
-        userDao1.dropUsersTable();
+        UserService userService = new UserServiceImpl();
+        userService.createUsersTable();
+        userService.saveUser("ivan","petrov", (byte) 24);
+        userService.saveUser("petr","ivanov", (byte) 14);
+        userService.saveUser("name1","petrov", (byte) 35);
+        userService.saveUser("ilya","sidorov", (byte) 18);
+        userService.removeUserById(1);
+        userService.getAllUsers();
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
